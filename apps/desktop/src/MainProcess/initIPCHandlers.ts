2fe9ac192e7f1765c56b84a6ff267f9001c48150
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-03 01:03:39 +0800

    WIP: revert temp fix

--- apps/desktop/src/MainProcess/initIPCHandlers.ts
+++ apps/desktop/src/MainProcess/initIPCHandlers.ts
@@ -1,5 +1,5 @@
 import { ipcMain, IpcMainInvokeEvent } from 'electron';
-// import { encryptAsync, decryptAsync } from "core";
+import { encryptAsync, decryptAsync } from "core";
 import { IPCRendererEvent } from '../@types/events';
 
 export function initIPCHandlers() {
@@ -7,16 +7,14 @@ export function initIPCHandlers() {
     ipcMain.handle(IPCRendererEvent.DecryptAsync, onDecryptAsync)
 }
 
-// TODO : Not sure why cannot create build when using local package...
-
 function onEncryptAsync(e: IpcMainInvokeEvent, password: string, inputPath: string) {
     const outputPath = getEncryptOutputPath(inputPath);
-    // encryptAsync(password, inputPath, outputPath);
+    encryptAsync(password, inputPath, outputPath);
 }
 
 function onDecryptAsync(e: IpcMainInvokeEvent, password: string, inputPath: string) {
     const outputPath = getDecryptOutputPath(inputPath);
-    // decryptAsync(password, inputPath, outputPath);
+    decryptAsync(password, inputPath, outputPath);
 }
 
 function getEncryptOutputPath(inputPath: string) {
