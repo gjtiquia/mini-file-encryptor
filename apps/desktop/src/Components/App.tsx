574678ceca1e844318f1c89d60adde8efcb8f273
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-02 12:54:06 +0800

    feat: setup tailwind and basic styling

--- apps/desktop/src/Components/App.tsx
+++ apps/desktop/src/Components/App.tsx
@@ -1,3 +1,23 @@
 export const App = () => {
-    return <h2>Hello from React!</h2>;
+    return <div className="bg-slate-800 h-full p-3 flex flex-col items-center justify-center gap-5">
+        <h1 className="font-bold text-4xl text-slate-200">
+            Mini File Encryptor
+        </h1>
+
+        <p className="text-slate-200">
+            // TODO : Encrypt/Decrypt Tab
+        </p>
+
+        <p className="text-slate-200">
+            // TODO : Drag and drop file into space
+        </p>
+
+        <p className="text-slate-200">
+            // TODO : Button to "Choose File"
+        </p>
+
+        <p className="text-slate-200">
+            // TODO : Encrypt Button
+        </p>
+    </div>;
 }
--- apps/desktop/src/renderer.ts
+++ apps/desktop/src/renderer.ts
@@ -2,6 +2,8 @@
  * This file will automatically be loaded by vite and run in the "renderer" context.
  */
 
+import "./index.css"
+
 // Add this to the end of the existing file
 // https://www.electronforge.io/guides/framework-integration/react-with-typescript
 import './react';
