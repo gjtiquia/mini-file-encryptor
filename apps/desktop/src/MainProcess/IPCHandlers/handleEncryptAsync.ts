218e234c4647c72830d473f6e876b430d02855a2
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-04 11:06:54 +0800

    feat: show item in folder after encryption

--- apps/desktop/src/MainProcess/IPCHandlers/handleEncryptAsync.ts
+++ apps/desktop/src/MainProcess/IPCHandlers/handleEncryptAsync.ts
@@ -1,9 +1,14 @@
-import { IpcMainInvokeEvent } from 'electron';
+import { IpcMainInvokeEvent, shell } from 'electron';
 import { encryptAsync } from "core";
 
 export async function handleEncryptAsync(e: IpcMainInvokeEvent, params: IEncryptParams) {
+    if (!params.password || !params.inputPath)
+        return;
+
     const outputPath = getEncryptOutputPath(params.inputPath);
     await encryptAsync(params.password, params.inputPath, outputPath);
+
+    shell.showItemInFolder(outputPath);
 }
 
 function getEncryptOutputPath(inputPath: string) {
