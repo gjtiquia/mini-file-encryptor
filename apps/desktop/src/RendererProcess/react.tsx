914a14dc2429301d334e745ab8eb72d9bc0432f6
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-04 11:11:01 +0800

    fix: null checking for app element to render React

--- apps/desktop/src/RendererProcess/react.tsx
+++ apps/desktop/src/RendererProcess/react.tsx
@@ -3,7 +3,13 @@ import { App } from './Components/App';
 
 export function createAndRenderRoot() {
     // https://react.dev/learn/add-react-to-an-existing-project#step-2-render-react-components-anywhere-on-the-page
-    const root = createRoot(document.getElementById('app'));
+
+    const appElement = document.getElementById('app');
+
+    if (appElement === null)
+        throw new Error("Cannot find element with id 'app'!")
+
+    const root = createRoot(appElement);
     root.render(<App />);
 }
 
