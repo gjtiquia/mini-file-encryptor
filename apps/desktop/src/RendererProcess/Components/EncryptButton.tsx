aeb3ef9f8d9133fbb1630b732b4ff78d0ec605e7
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-04 11:07:14 +0800

    feat: gray out button until can have input path and password

--- apps/desktop/src/RendererProcess/Components/EncryptButton.tsx
+++ apps/desktop/src/RendererProcess/Components/EncryptButton.tsx
@@ -4,16 +4,29 @@ interface IEncryptButtonProps {
 }
 
 export const EncryptButton = (props: IEncryptButtonProps) => {
+
+    function isEnabled() {
+        return props.password && props.inputPath;
+    }
+
     async function onClickAsync() {
         await window.electronAPI.encryptAsync({ password: props.password, inputPath: props.inputPath })
     }
 
     return <>
-        <button
-            className="text-slate-200 bg-slate-600 hover:bg-slate-700 active:bg-slate-900 px-10 py-1 rounded-lg font-bold"
-            onClick={onClickAsync}
-        >
-            Encrypt
-        </button>
+        {isEnabled() ?
+            <button
+                className="text-slate-200 bg-slate-600 hover:bg-slate-700 active:bg-slate-900 px-10 py-1 rounded-lg font-bold"
+                onClick={onClickAsync}
+            >
+                Encrypt
+            </button>
+            :
+            <button
+                className="text-slate-500 bg-slate-700 opacity-50 px-10 py-1 rounded-lg font-bold"
+            >
+                Encrypt
+            </button>
+        }
     </>
 }
