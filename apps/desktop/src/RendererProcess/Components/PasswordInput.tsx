32f3e5c006693fdc08000480f3b23654fcebcc35
GJ Tiquia <gershom.tiquia@gmail.com>
2024-01-04 23:41:14 +0800

    WIP: error on wrong password

--- apps/desktop/src/RendererProcess/Components/PasswordInput.tsx
+++ apps/desktop/src/RendererProcess/Components/PasswordInput.tsx
@@ -9,15 +9,20 @@ export const PasswordInput = (props: IPasswordInputProps) => {
         props.onChanged(passwordInput);
     }
 
-    return <div className="flex w-max gap-3">
-        <p className="text-slate-200">
-            Password:
-        </p>
-        <input
-            type="password"
-            className="block border px-1 rounded-lg cursor-pointer focus:outline-none text-slate-200 bg-slate-700 border-slate-600 placeholder-slate-400"
-            onChange={(e) => onPassworkInputChanged(e.target.value)}
-            value={props.value}
-        />
+    return <div>
+        <div className="flex w-max gap-3">
+            <p className="text-slate-200">
+                Password:
+            </p>
+            <input
+                type="password"
+                className="block border px-1 rounded-lg cursor-pointer focus:outline-none text-slate-200 bg-slate-700 border-slate-600 placeholder-slate-400"
+                onChange={(e) => onPassworkInputChanged(e.target.value)}
+                value={props.value}
+            />
+        </div>
+        {/* <span className="text-red-500">
+            Unable to decrypt. Are you sure you are using the correct password?
+        </span> */}
     </div>
 }
